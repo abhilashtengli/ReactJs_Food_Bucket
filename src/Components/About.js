efe3b7cb251f11c433500ea79fe069b055e07a5f
Abhilash Tengli <122851437+abhilashtengli@users.noreply.github.com>
2023-10-26 10:17:59 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import thali from "../Images/thali.jpg";
-import { linkedin_URL } from "./config";
+import { linkedin_URL } from "./Config";
 
 const About = () => {
   return (
